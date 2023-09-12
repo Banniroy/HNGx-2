b2888432fc459cdc50ea53bb24194f163ddc2f5a
Ajibade Abdulmatin Adekola <107342239+Banniroy@users.noreply.github.com>
2023-09-12 13:34:12 +0100

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,28 +1,13 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { useToasts } from "react-hot-toast";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart } from "@fortawesome/free-solid-svg-icons"; // Import your custom heart icon
+import { faHeart } from "@fortawesome/free-solid-svg-icons"; // Import the heart icon
 
 const Card = ({ posterBaseUrl, movie }) => {
   const [isFavorite, setIsFavorite] = useState(false);
-  const { addToast } = useToasts();
 
   const toggleFavorite = () => {
     setIsFavorite(!isFavorite);
-    if (isFavorite) {
-      // Show a toast notification when removing from favorites
-      addToast(`${movie.title} removed from favorites.`, {
-        icon: "❌", // You can customize the icon
-        appearance: "success",
-      });
-    } else {
-      // Show a toast notification when adding to favorites
-      addToast(`${movie.title} added to favorites!`, {
-        icon: "❤️", // You can customize the icon
-        appearance: "success",
-      });
-    }
   };
 
   return (
@@ -50,7 +35,7 @@ const Card = ({ posterBaseUrl, movie }) => {
             isFavorite ? "text-red-500" : "text-gray-400"
           }`}
         >
-          {/* Use your custom FontAwesome heart icon */}
+          {/* Use the FontAwesome heart icon */}
           <FontAwesomeIcon icon={faHeart} />
         </button>
       </div>
