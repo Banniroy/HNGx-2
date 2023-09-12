0ea89cba3e40cf225f063bf6399fa351871a6598
Ajibade Abdulmatin Adekola <107342239+Banniroy@users.noreply.github.com>
2023-09-12 15:29:39 +0100

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -11,35 +11,33 @@ const Card = ({ posterBaseUrl, movie }) => {
   };
 
   return (
-    <Link
-      to={`/movie/${movie.id}`}
-      className="grid text-gray-900 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"
-      data-testid="movie-card"
-    >
-      <img
-        src={`${posterBaseUrl}${movie.poster_path}`}
-        alt={movie.title}
-        data-testid="movie-poster"
-        className="h-auto max-w-xs pb-3"
-      />
-      <div className="flex flex-col pt-3">
-        <h2 data-testid="movie-title" className="font-semibold text-lg pb-3">
-          {movie.title}
-        </h2>
-        <p data-testid="movie-release-date" className="pb-2">
-          Release Date: {movie.release_date}
-        </p>
-        <button
-          onClick={toggleFavorite}
-          className={`text-xl cursor-pointer ${
-            isFavorite ? "text-red-500" : "text-gray-400"
-          }`}
-        >
-          {/* Use the FontAwesome heart icon */}
-          <FontAwesomeIcon icon={faHeart} />
-        </button>
-      </div>
-    </Link>
+    <div className="bg-white rounded-lg shadow-md">
+      <Link to={`/movie/${movie.id}`} data-testid="movie-link">
+        <img
+          src={`${posterBaseUrl}${movie.poster_path}`}
+          alt={movie.title}
+          className="w-full h-auto"
+          data-testid="movie-poster"
+        />
+        <div className="p-4">
+          <h2 className="text-xl font-semibold" data-testid="movie-title">
+            {movie.title}
+          </h2>
+          <p className="text-gray-600" data-testid="movie-release-date">
+            Release Date: {movie.release_date}
+          </p>
+        </div>
+      </Link>
+      <button
+        onClick={toggleFavorite}
+        className={`p-2 text-xl cursor-pointer ${
+          isFavorite ? 'text-red-500' : 'text-gray-400'
+        }`}
+        data-testid="favorite-button"
+      >
+        <FontAwesomeIcon icon={faHeart} className="heart-icon" data-testid="heart-icon" />
+      </button>
+    </div>
   );
 };
 
